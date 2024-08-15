07b848bffc4ab97ce289ff119e40323e34260394
Vijay Kumar <vijaykumarmganj1@gmail.com>
2024-08-15 15:28:12 +0530

    Today Dynamic Quotes Websites Created

---
+++ Wheather Api/app.js
@@ -0,0 +1,17 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '9b2f6a56ecmsh7be1759689fb8b1p10b665jsn40e27e487e16',
+		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
